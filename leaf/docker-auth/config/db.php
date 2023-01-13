c4885461aeb3755d54ec35afaac6e1fe2e2c440a
mychidarko <mickdd22@gmail.com>
2023-01-13 00:41:53 +0000

    feat: add db config

---
+++ leaf/docker-auth/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+// Connect to database. You only need to include what you want to configure
+db()->connect([
+    'dbtype' => 'mysql',
+    'port' => null,
+    'host' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
+    'dbname' => '',
+]);
