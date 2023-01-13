a8a88028ba31177aed7c942c07ec17e10c507755
mychidarko <mickdd22@gmail.com>
2023-01-13 00:40:13 +0000

    feat: add register route

--- leaf/auth/index.php
+++ leaf/auth/index.php
@@ -4,18 +4,40 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config/index.php';
 
 app()->get('/', function () {
-	response()->json('Welcome to app. Visit POST /login to login or POST /register to register');
+	response()->json('Welcome to app. Visit POST /auth/login to login or POST /auth/register to register');
 });
 
-app()->post('/login', function () {
-	$credentials = request()->get(['email', 'password']);
-	$user = auth()->login($credentials);
+app()->group('/auth', function () {
+	app()->post('/login', function () {
+		$credentials = request()->get(['email', 'password']);
+		$user = auth()->login($credentials);
 
-	if (!$user) {
-		response()->exit(auth()->errors(), 401);
-	}
+		if (!$user) {
+			response()->exit(['status' => 'error', 'data' => auth()->errors()], 401);
+		}
 
-	response()->json($user);
+		response()->json([
+			'status' => 'success',
+			'data' => $user
+		]);
+	});
+
+	app()->post('/register', function () {
+		$credentials = request()->get(['username', 'email', 'password']);
+		$user = auth()->register($credentials, ['username', 'email']);
+
+		if (!$user) {
+			response()->exit([
+				'status' => 'error',
+				'data' => auth()->errors()
+			], 401);
+		}
+
+		response()->json([
+			'status' => 'success',
+			'data' => $user
+		], 201);
+	});
 });
 
 app()->run();
