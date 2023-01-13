ad747c470fcc2ffdcb565e2b7c370b0182a65f21
mychidarko <mickdd22@gmail.com>
2023-01-13 00:35:14 +0000

    feat: add login route

--- leaf/auth/index.php
+++ leaf/auth/index.php
@@ -1,9 +1,21 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/config/index.php';
 
 app()->get('/', function () {
-	response()->page('./welcome.html');
+	response()->json('Welcome to app. Visit POST /login to login or POST /register to register');
+});
+
+app()->post('/login', function () {
+	$credentials = request()->get(['email', 'password']);
+	$user = auth()->login($credentials);
+
+	if (!$user) {
+		response()->exit(auth()->errors(), 401);
+	}
+
+	response()->json($user);
 });
 
 app()->run();
