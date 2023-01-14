7178a4e4a55e30e460037de7c762730c7f9123d3
mychidarko <mickdd22@gmail.com>
2023-01-14 09:49:45 +0000

    feat: add remote db

--- leaf/auth/config/db.php
+++ leaf/auth/config/db.php
@@ -1,11 +1,12 @@
 <?php
 
-// Connect to database. You only need to include what you want to configure
+// Connect to database. You only need to include what you want to configure.
+// A sample connection has been provided for a remote PostgreSQL database.
 db()->connect([
-    'dbtype' => 'mysql',
+    'dbtype' => 'pgsql',
     'port' => null,
-    'host' => '127.0.0.1',
+    'host' => 'dpg-cf01s7kgqg4cnpitvk3g-a.oregon-postgres.render.com',
     'username' => 'root',
-    'password' => '',
-    'dbname' => '',
+    'password' => '1eK3ogzJkM6JuuERRc3NuLZjFnYk8rab',
+    'dbname' => 'etest_27kz',
 ]);
