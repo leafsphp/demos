e1577405aa83f16928c1688bd7308edd3faead51
mychidarko <mickdd22@gmail.com>
2023-01-14 17:25:52 +0000

    feat: add OTP flow

--- leaf/auth/db/setup.sql
+++ leaf/auth/db/setup.sql
@@ -5,6 +5,17 @@ CREATE TABLE users (
     username varchar(255) NOT NULL UNIQUE,
     password varchar(255) NOT NULL,
     email varchar(255) NOT NULL UNIQUE,
+    verified_at timestamp,
     created_at timestamp NOT NULL DEFAULT now(),
     updated_at timestamp NOT NULL DEFAULT now()
+);
+
+CREATE TABLE otps (
+    id serial PRIMARY KEY,
+    user_id integer NOT NULL,
+    code varchar(255) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now(),
+    updated_at timestamp NOT NULL DEFAULT now(),
+    FOREIGN KEY (user_id)
+      REFERENCES users (user_id)
 );
